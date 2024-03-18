9af3a79ffbf5714576a2ed1319d0b1feb44deb91
Alexander Gerber <alex.gerber90@gmail.com>
2024-03-18 11:24:03 +0100

    rename show to list

--- src/CatalogsManager.php
+++ src/CatalogsManager.php
@@ -8,7 +8,7 @@ use Illuminate\Database\DatabaseManager;
 /**
  * @method int create(string $catName, string $catUser = null, string $catPassword=null, array $args=null)
  * @method int getPort(string $catName)
- * @method array show()
+ * @method array list()
  * @method bool drop(string $catName)
  */
 class CatalogsManager
